de4e357907db80279af151e052bff4c3e3037088
Saeed Rasooli <saeed.gnu@gmail.com>
2024-07-30 12:59:12 +0330

    add PtrValueSize for maps with pointer values

--- mapsize.go
+++ mapsize.go
@@ -18,3 +18,17 @@ func Size[K comparable, V any](m map[K]V) int64 {
 	bz := unsafe.Sizeof(bmap{}) + (keySize+valueSize)*bucketCnt
 	return int64(unsafe.Sizeof(hmap{}) + bz*bn)
 }
+
+// PtrValueSize returns how many bytes of memory a map occupies,
+// which is an approximation, but has reference value
+func PtrValueSize[K comparable, V any](m map[K]*V) int64 {
+	var zeroK K
+	var zeroValue V
+	keySize := unsafe.Sizeof(zeroK)
+	valueSize := unsafe.Sizeof(zeroValue)
+	vo := reflect.ValueOf(m)
+	hm := (*hmap)(unsafe.Pointer(vo.Pointer()))
+	bn := 1<<hm.B + uintptr(hm.noverflow)
+	bz := unsafe.Sizeof(bmap{}) + (keySize+valueSize)*bucketCnt
+	return int64(unsafe.Sizeof(hmap{})+bz*bn) + int64(len(m)*8)
+}
--- mapsize_test.go
+++ mapsize_test.go
@@ -1,14 +1,36 @@
 package mapsize
 
 import (
-	"fmt"
+	"math"
 	_ "net/http/pprof"
+	"testing"
 )
 
-func ExampleSize() {
+func TestSize(t *testing.T) {
 	m := make(map[int]struct{})
 	for i := 0; i < 100; i++ {
 		m[i] = struct{}{}
 	}
-	fmt.Println(Size(m)) // 1416
+	actual := Size(m)
+	expected := int64(1416)
+	if actual != expected {
+		t.Fatalf("actual=%v, expected=%v", actual, expected)
+	}
+}
+
+func TestPtrValueSize(t *testing.T) {
+	type V struct {
+		i int
+	}
+	m := make(map[int]*V)
+	for i := 0; i < 10000; i++ {
+		m[i] = &V{i: i}
+	}
+	actual := PtrValueSize(m)
+	expected := 376664
+	relError := math.Abs(1 - float64(expected)/float64(actual))
+	t.Logf("relError = %f", relError)
+	if relError > 0.01 {
+		t.Fatalf("actual=%v, expected=%v", actual, expected)
+	}
 }
